d2f8285ae59c17335b09735f203f5bec3d98e6b9
Michał <michal@winami.com>
2021-01-15 22:30:50 +0100

    Empty environment

---
+++ src/test/java/pl/javastart/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
