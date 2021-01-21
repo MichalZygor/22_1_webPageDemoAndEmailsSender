170c5d8e7906eeda9712d4f8e025fe6903b8071c
Michał <michal@winami.com>
2021-01-22 00:09:30 +0100

    working - without email

--- src/main/java/pl/javastart/webPageDemoAndEmailsSender/WebPageController.java
+++ src/main/java/pl/javastart/webPageDemoAndEmailsSender/WebPageController.java
@@ -4,17 +4,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.sql.rowset.Predicate;
+
 @Controller
 public class WebPageController {
+
     @GetMapping("/")
-    public String home(Model model){
+    public String home(Model model) {
         model.addAttribute("pagesList", WebPageList.values());
+        model.addAttribute("activePage", WebPageList.HOME_PAGE.getDescription());
         return "home";
     }
 
-    @GetMapping("/regulamin")
-    public String reg(Model model){
-        //model.addAttribute("pagesList", WebPageList.values());
-        return "regulamin";
+    @GetMapping("/HOME_PAGE")
+    public String pageHomeBis (){
+        return "redirect:/";
     }
+
+    @GetMapping("/WHY_VAZZA")
+    public String pageWhy(Model model) {
+        model.addAttribute("pagesList", WebPageList.values());
+        model.addAttribute("activePage", WebPageList.WHY_VAZZA.getDescription());
+        return "why_vazza";
+    }
+
+    @GetMapping("/VALUES")
+    public String pageValues(Model model) {
+        model.addAttribute("pagesList", WebPageList.values());
+        model.addAttribute("activePage", WebPageList.VALUES.getDescription());
+        return "values";
+    }
+
 }
